889a6d68e1b14423573d68c6e41ee6675fa50920
mileshkopetr <mileshko.petr@yandex.ru>
2020-08-17 15:19:00 +0300

    добавил классы
    написал варианты классов контроллеров

--- controllers/TraitControllers.php
+++ controllers/TraitControllers.php
@@ -1,17 +1,36 @@
 <?php
 
 /*
-
-     Если у контроллеров будут одинаковые методы пишем их тут
+	
+    	 Если у контроллеров будут одинаковые методы пишем их тут
     
 */
 namespace School\controllers;
 
 trait TraitControllers {
-    
+
+    # Start...    тестовй код    удалить после отладки
+
+    private $user = [
+        [
+         'name' => 'Peter',
+         'email' => 'peter@mail.ru',
+         'password' => '1234',
+         'access' => 'Учитель'
+        ],
+        [
+         'name' => 'Иван',
+         'email' => 'ivan@mail.ru',
+         'password' => '1234',
+         'access' => 'Ученик'
+        ],
+    ]; 
+
+	# End  ...    тестовый код
+
         public function action_any() {
             
-            
+            return 'Any called';
         }
     
 }
